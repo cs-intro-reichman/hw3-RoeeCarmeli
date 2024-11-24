9d5da9ad9887571601c4d991b61266e4c844a71f
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-25 00:36:36 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -11,14 +11,14 @@ public class Algebra {
 		//System.out.println(minus(2,7));  // 2 - 7
 		//System.out.println(times(-5,-5));  // 3 * 4
 		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-		System.out.println(pow(-5,3));      // 5^3
+		//System.out.println(pow(-5,3));      // 5^3
 		//System.out.println(pow(3,5));      // 3^5
 		//System.out.println(div(12,3));   // 12 / 3    
 		//System.out.println(div(5,5));    // 5 / 5  
 		//System.out.println(div(25,7));   // 25 / 7
 		//System.out.println(mod(25,7));   // 25 % 7
 		//System.out.println(mod(120,6));  // 120 % 6    
-		//System.out.println(sqrt(36));
+		System.out.println(sqrt(40));
 		//System.out.println(sqrt(263169));
 		//System.out.println(sqrt(76123));
 	}  
@@ -137,6 +137,7 @@ public class Algebra {
 			i++;
 			newNum = pow(i, 2);
 		}
+		if (newNum > x) i--;
 
 		return i;
 	}	  	  
